7e7a3cbdebae686b33fe8bb85e8e0476b0cae1c4
SairMezaG <z.meza.g@hotmail.com>
2024-09-20 22:49:40 -0500

    Se elimina el código comentado

--- tests/routes/recuperarPassword.tests.js
+++ tests/routes/recuperarPassword.tests.js
@@ -1,31 +1,3 @@
-/* const request = require('supertest');
-const app = require('../../app'); 
-
-describe('POST /api/recuperarPassword', () => {
-  it('debería enviar un correo de recuperación de contraseña si el usuario existe', async () => {
-    const response = await request(app)
-      .post('/api/recuperarPassword')
-      .send({ correo: 'z.meza.g@hotmail.com' });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe('Correo electrónico enviado');
-  });
-
-  it('debería devolver un error si el correo no existe', async () => {
-    const response = await request(app)
-      .post('/api/recuperarPassword')
-      .send({ correo: 'noexiste@hotmail.com' });
-
-    expect(response.statusCode).toBe(404);
-    expect(response.body.message).toBe('Usuario no encontrado');
-  });
-
-  afterAll(async () => {
-    
-    await mongoose.connection.close();
-  });
-}); */
-
 const request = require('supertest');
 const mongoose = require('mongoose'); 
 const app = require('../../app'); 
--- tests/routes/restablecerPassword.tests.js
+++ tests/routes/restablecerPassword.tests.js
@@ -1,64 +1,3 @@
-/* const request = require('supertest');
-const app = require('../../app'); // Ajusta la ruta según la ubicación de tu archivo app.js
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const { usuarioModel } = require('../../models'); // Ajusta la ruta según la ubicación de tu modelo
-
-let token;
-
-describe('POST /api/recuperarPassword', () => {
-  beforeAll(async () => {
-    // Solicitar restablecimiento de contraseña para obtener el token
-    const response = await request(app)
-      .post('/api/recuperarPassword')
-      .send({ correo: 'z.meza.g@hotmail.com' }); // Asegúrate de usar un correo válido en tu base de datos de pruebas
-
-    expect(response.statusCode).toBe(200);
-
-    // Obtener el token del correo enviado
-    const user = await usuarioModel.findOne({ correo: 'z.meza.g@hotmail.com' });
-    token = user.resetPasswordToken; // Usa el token real del usuario
-  });
-
-  it('debería restablecer la contraseña correctamente', async () => {
-    const response = await request(app)
-      .post(`/api/restablecerPassword/${token}`)
-      .send({ password: 'sair1234', confirmPassword: 'sair1234' });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe('Contraseña restablecida correctamente.');
-  });
-
-  it('debería devolver un error si el token es inválido o ha expirado', async () => {
-    // Simula un token inválido
-    const invalidToken = crypto.randomBytes(32).toString('hex');
-
-    const response = await request(app)
-      .post(`/api/restablecerPassword/${invalidToken}`)
-      .send({ password: 'sair1234', confirmPassword: 'sair1234' });
-
-    expect(response.statusCode).toBe(400);
-    expect(response.body.message).toBe('Token inválido o expirado.');
-  });
-
-  it('debería devolver un error si las contraseñas no coinciden', async () => {
-    const response = await request(app)
-      .post(`/api/restablecerPassword/${token}`)
-      .send({ password: 'Sair1234', confirmPassword: 'sair1234' });
-
-    expect(response.statusCode).toBe(400);
-    expect(response.body.message).toBe('Las contraseñas no coinciden.');
-  });
-
-  afterAll(async () => {
-    // Cerrar conexiones o limpiar recursos
-    await mongoose.connection.close();
-  });
-}); */
-
-
-
-
 const request = require('supertest');
 const app = require('../../app'); 
 const mongoose = require('mongoose');
@@ -68,7 +7,7 @@ let token;
 describe('POST /api/recuperarPassword', () => {
   beforeAll(async () => {
     
-    token = '332cb43488c81a677ef609cbc4ace042b085a8ed4b946341c09f45cbfa07b548'; // Pegar el token que recibes en tu correo
+    token = '332cb43488c81a677ef609cbc4ace042b085a8ed4b946341c09f45cbfa07b548'; 
   });
 
   it('debería restablecer la contraseña correctamente', async () => {
